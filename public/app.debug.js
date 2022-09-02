a03d97f441689b473cb632d7a5173188e59cfd17
Greg Munro <gregmunro90@gmail.com>
2022-09-02 13:21:19 +0200

    1

--- public/app.debug.js
+++ public/app.debug.js
@@ -37,16 +37,17 @@ class Bookmark {
           </div>
           <hr>
           <div class='public'>
-            <svg class='public-caret caret' xmlns='http://www.w3.org/2000/svg' viewbox='0 0 512 512'>
-              <title>Caret Down</title>
-              <path
-                d='M98 190.06l139.78 163.12a24 24 0 0036.44 0L414 190.06c13.34-15.57 2.28-39.62-18.22-39.62h-279.6c-20.5 0-31.56 24.05-18.18 39.62z' />
-            </svg>
-            <span class="heading">Public bookmarks <span id="publicCount">(0)</span></span>
-            <div id="public-placeholder" class="active"><p class='public-text'>You have no public bookmarks</p>
-            <p class='public-text'>Right-click on a bookmark and select 'Make public'.</p>
-            </div>
+              <svg class='public-caret caret' xmlns='http://www.w3.org/2000/svg' viewbox='0 0 512 512'>
+                <title>Caret Down</title>
+                <path d='M98 190.06l139.78 163.12a24 24 0 0036.44 0L414 190.06c13.34-15.57 2.28-39.62-18.22-39.62h-279.6c-20.5 0-31.56 24.05-18.18 39.62z' />
+              </svg>
+
+              <span class="heading">Public bookmarks <span id="publicCount">(0)</span></span>
+              <div id="public-placeholder" class="active"><p class='public-text'>You have no public bookmarks</p>
+              <p class='public-text'>Right-click on a bookmark and select 'Make public'.</p>
+            
           </div>
+        </div>
           <div class='my-bookmarks'>
             <svg class='myBookmarks-caret caret' xmlns='http://www.w3.org/2000/svg' viewbox='0 0 512 512'>
               <title>Caret Down</title>
